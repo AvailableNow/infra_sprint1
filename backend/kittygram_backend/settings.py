0a748bb020be04f1f18f8fff9cf20cdfa40d68fe
AvailableNow <curretly.out.of.service@gmail.com>
2023-06-28 22:04:23 +0000

    fix bug2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -91,11 +91,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = 'static_backend'
 
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_backend' 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
